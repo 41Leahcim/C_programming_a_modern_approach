e844b150c35189602a9bb0ee856e35a015a82da4
Michael Scholten <michael-scholten@hotmail.nl>
2025-01-25 19:22:19 +0100

    2.5 Computing the dimensional weight of a user defined box

--- app.c
+++ app.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void flush_stdin(){
+    int c;
+    do{
+        c = getchar();
+    }while(c != '\n' && c != EOF);
+}
+
+unsigned int read_box_dimension(const char *name){
+    unsigned int value = 0;
+    while(true){
+        printf("Enter %s of box: ", name);
+        if(scanf("%u", &value) == 0){
+            puts("Invalid value");
+            flush_stdin();
+        }else{
+            return value;
+        }
+    }
+}
 
 int main(){
-    const unsigned int height = 8, length = 12, width = 10;
+    unsigned int height, length, width;
+    height = read_box_dimension("height");
+    length = read_box_dimension("length");
+    width = read_box_dimension("width");
+    
     const unsigned int volume = height * length * width;
     const unsigned int weight = (volume + 165) / 166;
 
-    printf("Dimensions: %d x %d x %d\n", length, width, height);
     printf("Volume (cubic inches): %u\n", volume);
     printf("Dimensional weight (pounds): %u\n", weight);
 }
