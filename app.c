041a04c698e08d4b99904dabe0ee46c61e370113
Michael Scholten <michael-scholten@hotmail.nl>
2025-01-25 22:13:55 +0100

    Programming project 1 chapter 2

--- app.c
+++ app.c
@@ -1,37 +1,10 @@
-// Removed all unnecessary on line whitespace. Kept new lines as they only have impact and are
-// required with macros (only the 2 #include here)
-#include<stdio.h>
-#include<stdbool.h>
-
-void flush_stdin(){
-int c;
-do{
-c=getchar();
-}while(c!='\n'&&c!=EOF);
-}
-
-unsigned int read_box_dimension(const char*name){
-unsigned int value=0;
-while(true){
-printf("Enter %s of box: ",name);
-if(scanf("%u",&value)==0){
-puts("Invalid value");
-flush_stdin();
-}else{
-return value;
-}
-}
-}
+#include <stdio.h>
 
 int main(){
-unsigned int height,length,width;
-height=read_box_dimension("height");
-length=read_box_dimension("length");
-width=read_box_dimension("width");
-
-const unsigned int volume=height*length*width;
-const unsigned int weight=(volume+165)/166;
-
-printf("Volume (cubic inches): %u\n",volume);
-printf("Dimensional weight (pounds): %u\n",weight);
+    puts("       *");
+    puts("      *");
+    puts("     *");
+    puts("*   *");
+    puts(" * *");
+    puts("  *");
 }
