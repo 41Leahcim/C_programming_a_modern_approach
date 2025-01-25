48134e1e1fcfce27aa9e7528aabb5b1d1bb98d42
Michael Scholten <michael-scholten@hotmail.nl>
2025-01-25 21:16:18 +0100

    Chapter 2, exercise 1

--- app.c
+++ app.c
@@ -1,31 +1,6 @@
+/// No warnings
 #include <stdio.h>
-#include <stdbool.h>
-
-#define FREEZING_POINT 32.0
-
-void flush_stdin(){
-    int c;
-    do{
-        c = getchar();
-    }while(c != '\n' && c != EOF);
-}
-
-float read_temperature(){
-    float value = 0;
-    while(true){
-        printf("Enter Fahrenheit temperature: ");
-        if(scanf("%f", &value) == 0){
-            puts("Invalid value");
-            flush_stdin();
-        }else{
-            return value;
-        }
-    }
-}
-
-int main(){
-    const float fahrenheit = read_temperature();
-    const float scale_factor = 5.0 / 9.0;
-    const float celsius = (fahrenheit - FREEZING_POINT) * scale_factor;
-    printf("Celsius equivalent: %.1f\n", celsius);
+int main(void)
+{
+ printf("hello, world\n");
 }
