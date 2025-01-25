375a2a3910a545e91e5da572b86e98aa262bc108
Michael Scholten <michael-scholten@hotmail.nl>
2025-01-25 18:44:25 +0100

    2.4 Computing the dimensional weight of a box

--- app.c
+++ app.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    puts("To C, or not to C: that is the question.");
+    const unsigned int height = 8, length = 12, width = 10;
+    const unsigned int volume = height * length * width;
+    const unsigned int weight = (volume + 165) / 166;
+
+    printf("Dimensions: %d x %d x %d\n", length, width, height);
+    printf("Volume (cubic inches): %u\n", volume);
+    printf("Dimensional weight (pounds): %u\n", weight);
 }
