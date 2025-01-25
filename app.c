c10308c47ba866ebdb5a478ff2b7fc02895db07f
Michael Scholten <michael-scholten@hotmail.nl>
2025-01-25 22:10:26 +0100

    Section 2.8 exercise 10

--- app.c
+++ app.c
@@ -1,10 +1,37 @@
-#include <stdio.h>
+// Removed all unnecessary on line whitespace. Kept new lines as they only have impact and are
+// required with macros (only the 2 #include here)
+#include<stdio.h>
+#include<stdbool.h>
+
+void flush_stdin(){
+int c;
+do{
+c=getchar();
+}while(c!='\n'&&c!=EOF);
+}
+
+unsigned int read_box_dimension(const char*name){
+unsigned int value=0;
+while(true){
+printf("Enter %s of box: ",name);
+if(scanf("%u",&value)==0){
+puts("Invalid value");
+flush_stdin();
+}else{
+return value;
+}
+}
+}
 
 int main(){
-    int answer, q = 1, p = 3;
+unsigned int height,length,width;
+height=read_box_dimension("height");
+length=read_box_dimension("length");
+width=read_box_dimension("width");
 
-    // Tokens: Assign(answer, Div(Sub(Mul(3, q), Mul(p, p)), 3))
-    answer = (3 * q - p * p) / 3;
+const unsigned int volume=height*length*width;
+const unsigned int weight=(volume+165)/166;
 
-    printf("%d\n", answer);
+printf("Volume (cubic inches): %u\n",volume);
+printf("Dimensional weight (pounds): %u\n",weight);
 }
