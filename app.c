bbe4531318cb11d694ecc57afda54323d75a836a
Michael Scholten <michael-scholten@hotmail.nl>
2025-01-25 21:19:26 +0100

    Chapter 2, exercise 2

--- app.c
+++ app.c
@@ -1,6 +1,24 @@
-/// No warnings
+/**
+ * Outputs:
+ * Parkinson's Law:
+ * Work expands so as to fill the time
+ * available for its completion.
+*/
+
+// Directive
 #include <stdio.h>
+
 int main(void)
 {
- printf("hello, world\n");
+ // Statement
+ printf("Parkinson's Law:\nWork expands so as to ");
+
+ // Statement
+ printf("fill the time\n");
+
+ // Statement
+ printf("available for its completion.\n");
+
+ // statement
+ return 0;
 }
